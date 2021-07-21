b3fc41a6f1d753cab9901b49cb696f51c32f53a1
BBS12345 <85489351+BBS12345@users.noreply.github.com>
2021-07-21 18:06:08 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,10 @@
+var specification="Lyra";
+var details=" is a constellation."
+var all=specification.concat(details)
+var a=11;
+var b=21;
+var multiply=a*b;
+var subtract=a-b;
+console.log(all);
+console.log(multiply);
+console.log(subtract);
